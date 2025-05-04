e258c179f566ba1d492988dce89c12ff9c9344e1
Francisco Sales <franciscofsales1@gmail.com>
2025-05-04 09:37:00 +0100

    chore: add support for tumble dryer

--- src/services/homeAssistant.ts
+++ src/services/homeAssistant.ts
@@ -114,8 +114,8 @@ export class HomeAssistantService {
         identifiers: [applianceId],
         name: appliance.name || `Electrolux ${appliance.info.deviceType}`,
         manufacturer: 'Electrolux',
-        model: appliance.info.modelName || 'Unknown',
-        sw_version: '1.0.0',
+        model: `${appliance.info.modelName || 'Unknown'}${appliance.info.variant ? ` (${appliance.info.variant})` : ''}`,
+        sw_version: appliance.state?.applianceMainBoardSwVersion || appliance.state?.applianceUiSwVersion || '1.0.0',
         via_device: this.config.nodeId,
       };
 
@@ -156,6 +156,20 @@ export class HomeAssistantService {
       icon: 'mdi:wifi',
       component: 'binary_sensor',
     });
+    
+    // Create a device status sensor that aggregates key information
+    await this.createSensor({
+      applianceId,
+      deviceInfo,
+      name: 'Device Status',
+      uniqueId: `${applianceId}_device_status`,
+      stateTopic,
+      valueTemplate: appliance.info.deviceType === 'TUMBLE_DRYER'
+        ? '{{ value_json.applianceState + (value_json.doorState ? " (Door " + value_json.doorState + ")" : "") }}'
+        : '{{ value_json.status || value_json.applianceState || "Unknown" }}',
+      icon: 'mdi:information',
+      component: 'sensor',
+    });
 
     // Check for temperature and create temp sensor if available
     if (state.temperature !== undefined) {
@@ -187,45 +201,99 @@ export class HomeAssistantService {
       });
     }
 
-    // If the device is a washer/dryer or dishwasher, create program status sensor
+    // If the device is a washer/dryer, tumble dryer, or dishwasher, create program status sensor
     if (
       appliance.info.deviceType === 'WASHER' ||
       appliance.info.deviceType === 'DRYER' ||
-      appliance.info.deviceType === 'DISHWASHER'
+      appliance.info.deviceType === 'DISHWASHER' ||
+      appliance.info.deviceType === 'TUMBLE_DRYER'
     ) {
+      // Create program sensor based on device type
       await this.createSensor({
         applianceId,
         deviceInfo,
         name: 'Program',
         uniqueId: `${applianceId}_program`,
         stateTopic,
-        valueTemplate: '{{ value_json.program }}',
+        valueTemplate: appliance.info.deviceType === 'TUMBLE_DRYER' 
+          ? '{{ value_json.userSelections.programUID }}' 
+          : '{{ value_json.program }}',
         icon: 'mdi:washing-machine',
         component: 'sensor',
       });
 
+      // Create status sensor based on device type
       await this.createSensor({
         applianceId,
         deviceInfo,
         name: 'Status',
         uniqueId: `${applianceId}_status`,
         stateTopic,
-        valueTemplate: '{{ value_json.status }}',
+        valueTemplate: appliance.info.deviceType === 'TUMBLE_DRYER' 
+          ? '{{ value_json.applianceState }}' 
+          : '{{ value_json.status }}',
         icon: 'mdi:information-outline',
         component: 'sensor',
       });
 
       // Create remaining time sensor if available
-      if (state.remainingTime !== undefined) {
+      if (state.remainingTime !== undefined || (state.timeToEnd !== undefined && appliance.info.deviceType === 'TUMBLE_DRYER')) {
         await this.createSensor({
           applianceId,
           deviceInfo,
           name: 'Remaining Time',
           uniqueId: `${applianceId}_remaining_time`,
           stateTopic,
-          valueTemplate: '{{ value_json.remainingTime }}',
+          valueTemplate: appliance.info.deviceType === 'TUMBLE_DRYER' 
+            ? '{{ value_json.timeToEnd | int / 60 }}' 
+            : '{{ value_json.remainingTime }}',
           unitOfMeasurement: 'min',
           icon: 'mdi:timer-outline',
+          component: 'sensor',
+        });
+      }
+    }
+
+    // If the device is a tumble dryer, create specialized sensors
+    if (appliance.info.deviceType === 'TUMBLE_DRYER') {
+      // Door state sensor
+      if (state.doorState !== undefined) {
+        await this.createSensor({
+          applianceId,
+          deviceInfo,
+          name: 'Door State',
+          uniqueId: `${applianceId}_door_state`,
+          stateTopic,
+          valueTemplate: '{{ value_json.doorState }}',
+          icon: 'mdi:door',
+          component: 'sensor',
+        });
+      }
+
+      // Cycle phase sensor
+      if (state.cyclePhase !== undefined) {
+        await this.createSensor({
+          applianceId,
+          deviceInfo,
+          name: 'Cycle Phase',
+          uniqueId: `${applianceId}_cycle_phase`,
+          stateTopic,
+          valueTemplate: '{{ value_json.cyclePhase }}',
+          icon: 'mdi:washing-machine',
+          component: 'sensor',
+        });
+      }
+      
+      // Humidity target sensor
+      if (state.userSelections?.humidityTarget !== undefined) {
+        await this.createSensor({
+          applianceId,
+          deviceInfo,
+          name: 'Humidity Target',
+          uniqueId: `${applianceId}_humidity_target`,
+          stateTopic,
+          valueTemplate: '{{ value_json.userSelections.humidityTarget }}',
+          icon: 'mdi:water-percent',
           component: 'sensor',
         });
       }
@@ -274,6 +342,12 @@ export class HomeAssistantService {
         key === 'remainingTime' ||
         key === 'fridgeTemperature' ||
         key === 'freezerTemperature' ||
+        key === 'doorState' ||
+        key === 'timeToEnd' ||
+        key === 'cyclePhase' ||
+        key === 'applianceState' ||
+        key === 'userSelections' ||
+        key === 'device_status' ||
         typeof value === 'object'
       ) {
         continue;
